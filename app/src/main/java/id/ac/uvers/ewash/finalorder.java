04fbc723208e52819fb4f9e38140a07ec715bb7f
metta <mettamita22@gmail.com>
2022-06-15 02:30:20 +0700

    3rd update --sampai finalorder

--- app/src/main/java/id/ac/uvers/ewash/finalorder.java
+++ app/src/main/java/id/ac/uvers/ewash/finalorder.java
@@ -2,13 +2,99 @@ package id.ac.uvers.ewash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class finalorder extends AppCompatActivity {
 
+    private String userlaundryy, strnama, strkategori, strservice, strqbjh, strqbk, strqbc, strqsl, strqsl2, strqgr;
+    private String strhargabjh, strhargabk, strhargabc, strhargasl1, strhargasl2, strhargagr;
+
+    TextView namalaundry, namauser, alamatuser, kategori, service;
+    TextView qtybjh, qtybk, qtybc, qtyslb, qtyslk, qtygr;
+    TextView hargabjh, hargabk, hargabc, hargaslb, hargaslk, hargagr;
+
+    LinearLayout laybjhfinal, laybkfinal, laybcfinal, layslbfinal, layslkfinal, laygrfinal;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finalorder);
+
+        Intent order = getIntent();
+        strnama = order.getStringExtra("namalaundry");
+        strkategori = order.getStringExtra("kategori");
+        strservice = order.getStringExtra("service");
+        strqbjh = order.getStringExtra("bjhqty");
+        strqbk = order.getStringExtra("bkqty");
+        strqbc = order.getStringExtra("bcqty");
+        strqsl = order.getStringExtra("slqty");
+        strqsl2 = order.getStringExtra("slqty2");
+        strqgr = order.getStringExtra("grqty");
+        strhargabjh = order.getStringExtra("hargabjh");
+        strhargabk = order.getStringExtra("hargabk");
+        strhargabc = order.getStringExtra("hargabc");
+        strhargasl1 = order.getStringExtra("hargasl1");
+        strhargasl2 = order.getStringExtra("hargasl2");
+        strhargagr = order.getStringExtra("hargagr");
+
+        namalaundry = findViewById(R.id.namalaundryfinal);
+        namauser = findViewById(R.id.namauserfinal);
+        alamatuser = findViewById(R.id.alamatuserfinal);
+        kategori = findViewById(R.id.kategorifinal);
+        service = findViewById(R.id.servicefinal);
+
+        /*qty*/
+        qtybjh = findViewById(R.id.bjhqtyfinal);
+        qtybk = findViewById(R.id.bkqtyfinal);
+        qtybc = findViewById(R.id.bcqtyfinal);
+        qtyslb = findViewById(R.id.slbqtyfinal);
+        qtyslk = findViewById(R.id.slkqtyfinal);
+        qtygr = findViewById(R.id.grqtyfinal);
+
+        /*harga*/
+        hargabjh = findViewById(R.id.thbjhfinal);
+        hargabk = findViewById(R.id.thbkfinal);
+        hargabc = findViewById(R.id.thbcfinal);
+        hargaslb = findViewById(R.id.thslbfinal);
+        hargaslk = findViewById(R.id.thslkfinal);
+        hargagr = findViewById(R.id.thgrfinal);
+
+        /*layout*/
+        laybjhfinal = findViewById(R.id.laybjhfinal);
+        laybkfinal = findViewById(R.id.laybkfinal);
+        laybcfinal = findViewById(R.id.laybcfinal);
+        layslbfinal = findViewById(R.id.layslbfinal);
+        layslkfinal = findViewById(R.id.layslkfinal);
+        laygrfinal = findViewById(R.id.laygrfinal);
+
+        /*set*/
+        namalaundry.setText(strnama);
+//        namauser.setText();
+//        alamatuser.setText();
+        kategori.setText(strkategori);
+        service.setText(strservice);
+
+        /*set qty*/
+        qtybjh.setText(strqbjh);
+        qtybk.setText(strqbk);
+        qtybc.setText(strqbc);
+        qtyslb.setText(strqsl);
+        qtyslk.setText(strqsl2);
+        qtygr.setText(strqgr);
+
+        /*set harga*/
+        hargabjh.setText(strhargabjh);
+        hargabk.setText(strhargabk);
+        hargabc.setText(strhargabc);
+        hargaslb.setText(strhargasl1);
+        hargaslk.setText(strhargasl2);
+        hargagr.setText(strhargagr);
+
     }
 }
--- app/src/main/java/id/ac/uvers/ewash/orderlaundry.java
+++ app/src/main/java/id/ac/uvers/ewash/orderlaundry.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +31,7 @@ public class orderlaundry extends AppCompatActivity {
 
     private String namalaundry, userlaundry;
     TextView detailnamalaundry, kategorijs, services, userldry;
+    private SwipeRefreshLayout refreshdata;
 
     ImageView back, info, bjhmin, bjhplus, bkmin, bkplus, bcmin, bcplus, slmin, slplus, slmin2, slplus2, grmin, grplus;
     Button order;
@@ -37,7 +39,10 @@ public class orderlaundry extends AppCompatActivity {
     ScrollView scorderl;
 
     private String userlaundryy, strnama, strkategori, strservice, strqbjh, strqbk, strqbc, strqsl, strqsl2, strqgr;
+    private String strhargabjh, strhargabk, strhargabc, strhargasl1, strhargasl2, strhargagr;
     private TextView hargabjh, hargabk, hargabc, hargasl1, hargasl2, hargagr;
+    private TextView bjhharga, bkharga, bcharga, slbharga, slkharga, grharga;
+    int bjhtharga,bktharga,bctharga,slbtharga,slktharga,grtharga;
 
     int ibjhqty = 0, ibkqty = 0, ibcqty = 0, islqty = 0, islqty2 = 0, igrqty = 0;
     //    private orderlaundryAdapter olAdapter;
@@ -61,12 +66,23 @@ public class orderlaundry extends AppCompatActivity {
         kategorijs = findViewById(R.id.kategori);
         services = findViewById(R.id.service);
         userldry = findViewById(R.id.userldry);
+        refreshdata = findViewById(R.id.swprf);
 
         /*Qty Text*/
         bjhqty = findViewById(R.id.bjhqty);
+        bkqty = findViewById(R.id.bkqty);
+        bcqty = findViewById(R.id.bcqty);
+        slqty = findViewById(R.id.slqty1);
+        slqty2 = findViewById(R.id.slqty2);
+        grqty = findViewById(R.id.grqty);
 
         /*Set Qty*/
         bjhqty.setText(String.valueOf(ibjhqty));
+        bkqty.setText(String.valueOf(ibkqty));
+        bcqty.setText(String.valueOf(ibcqty));
+        slqty.setText(String.valueOf(islqty));
+        slqty2.setText(String.valueOf(islqty2));
+        grqty.setText(String.valueOf(igrqty));
 
         /*Plus and Minus Buttons*/
         bjhmin = findViewById(R.id.bjhmin);
@@ -89,6 +105,14 @@ public class orderlaundry extends AppCompatActivity {
         hargasl1 = findViewById(R.id.hargasld1);
         hargasl2 = findViewById(R.id.hargasld2);
         hargagr = findViewById(R.id.hargagrd);
+
+        /*Text View only for simpan harga sementara*/
+        bjhharga = findViewById(R.id.bjhharga);
+        bkharga = findViewById(R.id.bkharga);
+        bcharga = findViewById(R.id.bcharga);
+        slbharga = findViewById(R.id.slbharga);
+        slkharga = findViewById(R.id.slkharga);
+        grharga = findViewById(R.id.grharga);
 
         /*Linear Layouts*/
         LinearLayout lyboneka = findViewById(R.id.lyboneka);
@@ -117,9 +141,16 @@ public class orderlaundry extends AppCompatActivity {
         // get product list layout
         productlist = findViewById(R.id.productlist);
 
-//        Log.d("UVEEEEEEEEEERS", service);
+        refreshdata.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshdata.setRefreshing(true);
+                get_current_laundry(kategori, service);
+                refreshdata.setRefreshing(false);
+            }
+        });
 
-        get_current_laundry(kategori, service);
+//        Log.d("UVEEEEEEEEEERS", service);
 
 //        tp_harga();
 //        oladapter2 = new orderlaundryAdapter2(orderlaundry.this, laundryprice);
@@ -157,14 +188,34 @@ public class orderlaundry extends AppCompatActivity {
                 strnama = detailnamalaundry.getText().toString().trim();
                 strkategori = kategorijs.getText().toString().trim();
                 strservice = services.getText().toString().trim();
+                strqbjh = bjhqty.getText().toString().trim();
+                strqbk = bkqty.getText().toString().trim();
+                strqbc = bcqty.getText().toString().trim();
+                strqsl = slqty.getText().toString().trim();
+                strqsl2 = slqty2.getText().toString().trim();
+                strqgr = grqty.getText().toString().trim();
+                strhargabjh = hargabjh.getText().toString().trim();
+                strhargabk = hargabk.getText().toString().trim();
+                strhargabc = hargabc.getText().toString().trim();
+                strhargasl1 = hargasl1.getText().toString().trim();
+                strhargasl2 = hargasl2.getText().toString().trim();
+                strhargagr = hargagr.getText().toString().trim();
                 Intent order = new Intent(getApplicationContext(), finalorder.class);
-                order.putExtra("nama", strnama);
+                order.putExtra("namalaundry", strnama);
                 order.putExtra("kategori", strkategori);
                 order.putExtra("service", strservice);
                 order.putExtra("bjhqty", strqbjh);
                 order.putExtra("bkqty", strqbk);
                 order.putExtra("bcqty", strqbc);
                 order.putExtra("slqty", strqsl);
+                order.putExtra("slqty2", strqsl2);
+                order.putExtra("grqty", strqgr);
+                order.putExtra("hargabjh", strhargabjh);
+                order.putExtra("hargabk", strhargabk);
+                order.putExtra("hargabc", strhargabc);
+                order.putExtra("hargasl1", strhargasl1);
+                order.putExtra("hargasl2", strhargasl2);
+                order.putExtra("hargagr", strhargagr);
                 startActivity(order);
             }
         });
@@ -186,7 +237,7 @@ public class orderlaundry extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (ibjhqty > 0 || ibjhqty == 0) {
-                    if (ibjhqty <= 50) {
+                    if (ibjhqty <= 30) {
                         bjhplus.setClickable(true);
                         ibjhqty = ibjhqty + 1;
                         bjhqty.setText(String.valueOf(ibjhqty));
@@ -196,6 +247,161 @@ public class orderlaundry extends AppCompatActivity {
                     }
                 } else if (ibjhqty < 0) {
                     bjhplus.setClickable(false);
+                }
+            }
+        });
+
+        bkmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ibkqty > 0) {
+                    bkmin.setClickable(true);
+                    ibkqty = ibkqty - 1;
+                    bkqty.setText(String.valueOf(ibkqty));
+                } else if (ibkqty == 0) {
+                    bkmin.setClickable(false);
+                }
+            }
+        });
+
+        bkplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ibkqty > 0 || ibkqty == 0) {
+                    if (ibkqty <= 10) {
+                        bkplus.setClickable(true);
+                        ibkqty = ibkqty + 1;
+                        bkqty.setText(String.valueOf(ibkqty));
+                    } else {
+                        bkplus.setClickable(false);
+                        Toast.makeText(orderlaundry.this, "TELAH MELEBIHI BATS MAKSIMUM", Toast.LENGTH_LONG).show();
+                    }
+                } else if (ibkqty < 0) {
+                    bkplus.setClickable(false);
+                }
+            }
+        });
+
+        bcmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ibcqty > 0) {
+                    bcmin.setClickable(true);
+                    ibcqty = ibcqty - 1;
+                    bcqty.setText(String.valueOf(ibcqty));
+                } else if (ibcqty == 0) {
+                    bcmin.setClickable(false);
+                }
+            }
+        });
+
+        bcplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ibcqty > 0 || ibcqty == 0) {
+                    if (ibcqty <= 10) {
+                        bcplus.setClickable(true);
+                        ibcqty = ibcqty + 1;
+                        bcqty.setText(String.valueOf(ibcqty));
+                    } else {
+                        bcplus.setClickable(false);
+                        Toast.makeText(orderlaundry.this, "TELAH MELEBIHI BATS MAKSIMUM", Toast.LENGTH_LONG).show();
+                    }
+                } else if (ibcqty < 0) {
+                    bcplus.setClickable(false);
+                }
+            }
+        });
+
+        slmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (islqty > 0) {
+                    slmin.setClickable(true);
+                    islqty = islqty - 1;
+                    slqty.setText(String.valueOf(islqty));
+                } else if (islqty == 0) {
+                    slmin.setClickable(false);
+                }
+            }
+        });
+
+        slplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (islqty > 0 || islqty == 0) {
+                    if (islqty <= 10) {
+                        slplus.setClickable(true);
+                        islqty = islqty + 1;
+                        slqty.setText(String.valueOf(islqty));
+                    } else {
+                        slplus.setClickable(false);
+                        Toast.makeText(orderlaundry.this, "TELAH MELEBIHI BATS MAKSIMUM", Toast.LENGTH_LONG).show();
+                    }
+                } else if (islqty < 0) {
+                    slplus.setClickable(false);
+                }
+            }
+        });
+
+        slmin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (islqty2 > 0) {
+                    slmin2.setClickable(true);
+                    islqty2 = islqty2 - 1;
+                    slqty2.setText(String.valueOf(islqty2));
+                } else if (islqty2 == 0) {
+                    slmin2.setClickable(false);
+                }
+            }
+        });
+
+        slplus2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (islqty2 > 0 || islqty2 == 0) {
+                    if (islqty2 <= 10) {
+                        slplus2.setClickable(true);
+                        islqty2 = islqty2 + 1;
+                        slqty2.setText(String.valueOf(islqty2));
+                    } else {
+                        slplus2.setClickable(false);
+                        Toast.makeText(orderlaundry.this, "TELAH MELEBIHI BATS MAKSIMUM", Toast.LENGTH_LONG).show();
+                    }
+                } else if (islqty2 < 0) {
+                    slplus2.setClickable(false);
+                }
+            }
+        });
+
+        grmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (igrqty > 0) {
+                    grmin.setClickable(true);
+                    igrqty = igrqty - 1;
+                    grqty.setText(String.valueOf(igrqty));
+                } else if (igrqty == 0) {
+                    grmin.setClickable(false);
+                }
+            }
+        });
+
+        grplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (igrqty > 0 || igrqty == 0) {
+                    if (igrqty <= 50) {
+                        grplus.setClickable(true);
+                        igrqty = igrqty + 1;
+                        grqty.setText(String.valueOf(igrqty));
+                    } else {
+                        grplus.setClickable(false);
+                        Toast.makeText(orderlaundry.this, "TELAH MELEBIHI BATS MAKSIMUM", Toast.LENGTH_LONG).show();
+                    }
+                } else if (igrqty < 0) {
+                    grplus.setClickable(false);
                 }
             }
         });
@@ -225,23 +431,101 @@ public class orderlaundry extends AppCompatActivity {
 //                hargagr.setText(current_laundry.getGordenE());
 
                 if (kategori.equals("Normal") && service.equals("Complete")) {
-                    hargabjh.setText(current_laundry.getBajuN());
-                    hargabk.setText(current_laundry.getBonekaN());
-                    hargabc.setText(current_laundry.getBedcoverN());
-                    hargasl1.setText(current_laundry.getSelimutbN());
-                    hargasl2.setText(current_laundry.getSelimutkN());
-                    hargagr.setText(current_laundry.getGordenN());
+                    bjhharga.setText(current_laundry.getBajuN());
+                    bkharga.setText(current_laundry.getBonekaN());
+                    bcharga.setText(current_laundry.getBedcoverN());
+                    slbharga.setText(current_laundry.getSelimutbN());
+                    slkharga.setText(current_laundry.getSelimutkN());
+                    grharga.setText(current_laundry.getGordenN());
+
+                    String qtybjh = bjhqty.getText().toString();
+                    String qtybk = bkqty.getText().toString();
+                    String qtybc = bcqty.getText().toString();
+                    String qtyslb =slqty.getText().toString();
+                    String qtyslk =slqty2.getText().toString();
+                    String qtygr = grqty.getText().toString();
+
+                    String bjhharga1 = bjhharga.getText().toString().trim();
+                    String bkharga1 = bkharga.getText().toString().trim();
+                    String bcharga1 = bcharga.getText().toString().trim();
+                    String slbharga1 =slbharga.getText().toString().trim();
+                    String slkharga1 =slkharga.getText().toString().trim();
+                    String grharga1 = grharga.getText().toString().trim();
+
+                    /* total harga */
+                    bjhtharga = Integer.parseInt(qtybjh)*Integer.parseInt(bjhharga1);
+                    bktharga = Integer.parseInt(qtybk)*Integer.parseInt(bkharga1);
+                    bctharga = Integer.parseInt(qtybc)*Integer.parseInt(bcharga1);
+                    slbtharga =Integer.parseInt(qtyslb)*Integer.parseInt(slbharga1);
+                    slktharga =Integer.parseInt(qtyslk)*Integer.parseInt(slkharga1);
+                    grtharga = Integer.parseInt(qtygr)*Integer.parseInt(grharga1);
+
+                    hargabjh.setText(String.valueOf(bjhtharga));
+                    hargabk.setText(String.valueOf(bktharga));
+                    hargabc.setText(String.valueOf(bctharga));
+                    hargasl1.setText(String.valueOf(slbtharga));
+                    hargasl2.setText(String.valueOf(slktharga));
+                    hargagr.setText(String.valueOf(grtharga));
+
                 } else if (kategori.equals("Express") && service.equals("Complete")) {
-                    hargabjh.setText(current_laundry.getBajuE());
-                    hargabk.setText(current_laundry.getBonekaE());
-                    hargabc.setText(current_laundry.getBedcoverE());
-                    hargasl1.setText(current_laundry.getSelimutbE());
-                    hargasl2.setText(current_laundry.getSelimutkE());
-                    hargagr.setText(current_laundry.getGordenE());
+                    bjhharga.setText(current_laundry.getBajuE());
+                    bkharga.setText(current_laundry.getBonekaE());
+                    bcharga.setText(current_laundry.getBedcoverE());
+                    slbharga.setText(current_laundry.getSelimutbE());
+                    slkharga.setText(current_laundry.getSelimutkE());
+                    grharga.setText(current_laundry.getGordenE());
+
+                    String qtybjh = bjhqty.getText().toString();
+                    String qtybk = bkqty.getText().toString();
+                    String qtybc = bcqty.getText().toString();
+                    String qtyslb =slqty.getText().toString();
+                    String qtyslk =slqty2.getText().toString();
+                    String qtygr = grqty.getText().toString();
+
+                    String bjhharga1 = bjhharga.getText().toString().trim();
+                    String bkharga1 = bkharga.getText().toString().trim();
+                    String bcharga1 = bcharga.getText().toString().trim();
+                    String slbharga1 =slbharga.getText().toString().trim();
+                    String slkharga1 =slkharga.getText().toString().trim();
+                    String grharga1 = grharga.getText().toString().trim();
+
+                    /* total harga */
+                    bjhtharga = Integer.parseInt(qtybjh)*Integer.parseInt(bjhharga1);
+                    bktharga = Integer.parseInt(qtybk)*Integer.parseInt(bkharga1);
+                    bctharga = Integer.parseInt(qtybc)*Integer.parseInt(bcharga1);
+                    slbtharga =Integer.parseInt(qtyslb)*Integer.parseInt(slbharga1);
+                    slktharga =Integer.parseInt(qtyslk)*Integer.parseInt(slkharga1);
+                    grtharga = Integer.parseInt(qtygr)*Integer.parseInt(grharga1);
+
+                    hargabjh.setText(String.valueOf(bjhtharga));
+                    hargabk.setText(String.valueOf(bktharga));
+                    hargabc.setText(String.valueOf(bctharga));
+                    hargasl1.setText(String.valueOf(slbtharga));
+                    hargasl2.setText(String.valueOf(slktharga));
+                    hargagr.setText(String.valueOf(grtharga));
                 } else if (kategori.equals("Normal") && service.equals("Setrika")) {
-                    hargabjh.setText((current_laundry.getSetrikaN()));
+                    bjhharga.setText((current_laundry.getSetrikaN()));
+
+                    String qtybjh = bjhqty.getText().toString();
+
+                    String bjhharga1 = bjhharga.getText().toString().trim();
+
+                    /* total harga */
+                    bjhtharga = Integer.parseInt(qtybjh)*Integer.parseInt(bjhharga1);
+
+                    hargabjh.setText(String.valueOf(bjhtharga));
+
                 } else if (kategori.equals("Express") && service.equals("Setrika")) {
-                    hargabjh.setText(current_laundry.getSetrikaE());
+                    bjhharga.setText(current_laundry.getSetrikaE());
+
+                    String qtybjh = bjhqty.getText().toString();
+
+                    String bjhharga1 = bjhharga.getText().toString().trim();
+
+                    /* total harga */
+                    bjhtharga = Integer.parseInt(qtybjh)*Integer.parseInt(bjhharga1);
+
+                    hargabjh.setText(String.valueOf(bjhtharga));
                 }
             }
 
